c3dd8800d58acc4d4a168868b7aa7abdeda4c5b7
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2023-12-18 17:54:50 +0800

    12/18/2023 - 17:54
    added new page

---
+++ test/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".hero_list1").click(function(){
+       $(".hero").fadeOut(1000);
+       $(".menubar").fadeToggle(3000);
+       $(".projects").fadeIn(3000);
+   });
+   });
+
+ $(document).ready(function(){
+   $(".backbtn").click(function(){
+       $(".projects").fadeOut(1000);
+       $(".menubar").fadeOut(1000);
+       $(".hero").fadeIn(1000);
+   });
+   });
+
+ 
+
