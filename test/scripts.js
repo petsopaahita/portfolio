858279cf54c459d1758baa9f078d9e658f0b9ea4
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2023-12-20 17:56:41 +0800

    12/20/2023 - 17:56
    weweaweasdasdw

--- test/scripts.js
+++ test/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
        $(".hero").fadeOut(1000);
        $(".menubar").fadeToggle(3000);
        $(".projects").fadeIn(3000);
+       $("#main").css("background-image", "url('')");
    });
    });
 
@@ -11,6 +12,7 @@ $(document).ready(function(){
        $(".projects").fadeOut(1000);
        $(".menubar").fadeOut(1000);
        $(".hero").fadeIn(1000);
+       $("#main").css("background-image", "");
    });
    });
 
