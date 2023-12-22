196684f9598eefadbbb7864fd699e81eb971614c
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2023-12-22 16:49:27 +0800

    12/22/2023 - 16:49
    asdasdasd

--- test/scripts.js
+++ test/scripts.js
@@ -1,19 +1,37 @@
 $(document).ready(function(){
   $(".hero_list1").click(function(){
       $(".hero").fadeOut(1000);
-      $(".menubar").fadeToggle(3000);
+      $(".menubar").fadeIn(3000);
       $(".projects").fadeIn(3000);
       $('#particles-js').css('transform', 'scale(5)');
       $("#particles-js").fadeToggle(1000);
       $("#particles-js").removeClass("clicked");
-      $("#particles-js-2").fadeIn(1000);
     });
  });
- 
+$(document).ready(function(){
+  $(".hero_list2").click(function(){
+      $(".hero").fadeOut(1000);
+      $(".menubar").fadeIn(3000);
+      $(".profile").fadeIn(3000);
+      $('#particles-js').css('transform', 'scale(5)');
+      $("#particles-js").fadeToggle(1000);
+      $("#particles-js").removeClass("clicked");
+    });
+ });
+ $(document).ready(function(){
+   $(".hero_list3").click(function(){
+       $(".hero").fadeOut(1000);
+       $(".menubar").fadeIn(3000);
+       $(".contact").fadeIn(3000);
+       $('#particles-js').css('transform', 'scale(5)');
+       $("#particles-js").fadeToggle(1000);
+       $("#particles-js").removeClass("clicked");
+     });
+  });
  $(document).ready(function(){
   $(".backbtn").click(function(){
-       $(".projects").fadeOut(1000);
-       $(".menubar").fadeOut(1000);
+       $(".projects, .profile, .contact").fadeOut(1);
+       $(".menubar").fadeOut(1);
        $(".hero").fadeIn(1000);
        $("#particles-js").addClass("clicked");
        $("#particles-js").fadeIn(2000);
