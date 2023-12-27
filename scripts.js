3d2183431d1119ec8e0aaee42f6b6ed2ab58f865
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2023-12-27 17:33:49 +0800

    12/27/2023 - 17:33
    Made more changes

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $(".hero_list1").click(function(){
       $(".hero").fadeOut(1000);
       $(".menubar").fadeIn(3000);
+      $(".menubar1").hide(0);
+      $(".menubar2").show(0);
+      $(".menubar3").show(0);
       $(".projects").fadeIn(3000);
       $('#particles-js').css('transform', 'scale(5)');
       $("#particles-js").fadeToggle(1000);
@@ -12,6 +15,9 @@ $(document).ready(function(){
   $(".hero_list2").click(function(){
       $(".hero").fadeOut(1000);
       $(".menubar").fadeIn(3000);
+      $(".menubar1").show(0);
+      $(".menubar2").hide(0);
+      $(".menubar3").show(0);      
       $(".profile").fadeIn(3000);
       $('#particles-js').css('transform', 'scale(5)');
       $("#particles-js").fadeToggle(1000);
@@ -22,6 +28,9 @@ $(document).ready(function(){
    $(".hero_list3").click(function(){
        $(".hero").fadeOut(1000);
        $(".menubar").fadeIn(3000);
+       $(".menubar1").show(0);
+       $(".menubar2").show(0);
+       $(".menubar3").hide(0);       
        $(".contact").fadeIn(3000);
        $('#particles-js').css('transform', 'scale(5)');
        $("#particles-js").fadeToggle(1000);
@@ -39,3 +48,42 @@ $(document).ready(function(){
        $("#main").css("z-index", "2");
     });
  });
+ $(document).ready(function(){
+  $(".menubar1").click(function(){
+    $(".menubar1").hide(0);
+    $(".menubar2").show(0);
+    $(".menubar3").show(0);
+    $(".projects").fadeIn(3000);
+    $(".profile, .contact").fadeOut(1);
+    $(".menubar").fadeOut(1, function(){
+      $(".menubar").fadeIn(3000);
+    });
+    });
+ });
+ $(document).ready(function(){
+  $(".menubar2").click(function(){
+    $(".menubar1").show(0);
+    $(".menubar2").hide(0);
+    $(".menubar3").show(0);
+    $(".profile").fadeIn(3000);
+    $(".projects, .contact").fadeOut(1);
+    $(".menubar").fadeOut(1, function(){
+      $(".menubar").fadeIn(3000);
+    });
+    });
+ });
+ $(document).ready(function(){
+  $(".menubar3").click(function(){
+    $(".menubar1").show(0);
+    $(".menubar2").show(0);
+    $(".menubar3").hide(0);
+    $(".contact").fadeIn(3000);
+    $(".projects, .profile").fadeOut(1);
+    $(".menubar").fadeOut(1, function(){
+      $(".menubar").fadeIn(3000);
+    });
+    });
+ });
+
+
+
