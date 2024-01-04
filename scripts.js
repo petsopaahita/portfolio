308d484564c42c86cd968f8ab0cb8be22d095d81
petsopaahita <114787997+petsopaahita@users.noreply.github.com>
2024-01-04 17:52:48 +0800

    1/4/2024 - 17:49
    made adjustments in menubar

--- scripts.js
+++ scripts.js
@@ -2,47 +2,49 @@ $(document).ready(function(){
   $(".hero_list1").click(function(){
       $(".hero").fadeOut(1);
       $(".hero2").fadeOut(250);
-      $(".menubar").fadeIn(2000);
+      $(".menubar").fadeIn(1000);
       $(".header").fadeOut(1);
-      $(".menubar1").hide(0);
-      $(".menubar2").show(0);
-      $(".menubar3").show(0);
-      $(".projects").fadeIn(2000);
+      $(".projects").fadeIn(1000);
       $('#particles-js').css('transform', 'scale(5)');
       $("#particles-js").fadeToggle(1000);
       $("#particles-js").removeClass("clicked");
+      $(".menubar").css("display", "flex");
     });
  });
 $(document).ready(function(){
   $(".hero_list2").click(function(){
       $(".hero").fadeOut(1);
       $(".hero2").fadeOut(250);
-      $(".menubar").fadeIn(2000);
-      $(".header").fadeOut(1);
-      $(".menubar1").show(0);
-      $(".menubar2").hide(0);
-      $(".menubar3").show(0);      
-      $(".profile").fadeIn(2000);
+      $(".menubar").fadeIn(1000);
+      $(".header").fadeOut(1);   
+      $(".profile").fadeIn(1000);
       $('#particles-js').css('transform', 'scale(5)');
       $("#particles-js").fadeToggle(1000);
       $("#particles-js").removeClass("clicked");
+      $(".menubar").css("display", "flex");
     });
  });
  $(document).ready(function(){
    $(".hero_list3").click(function(){
        $(".hero").fadeOut(1);
        $(".hero2").fadeOut(250);
-       $(".menubar").fadeIn(2000);
-       $(".header").fadeOut(1);
-       $(".menubar1").show(0);
-       $(".menubar2").show(0);
-       $(".menubar3").hide(0);       
-       $(".contact").fadeIn(2000);
+       $(".menubar").fadeIn(1000);
+       $(".header").fadeOut(1);      
+       $(".contact").fadeIn(1000);
        $('#particles-js').css('transform', 'scale(5)');
        $("#particles-js").fadeToggle(1000);
        $("#particles-js").removeClass("clicked");
+       $(".menubar").css("display", "flex");
      });
   });
+ $(document).ready(function(){
+  $(".menu").click(function(){
+    $(".xpnd_menu").fadeIn(500);
+    $(".xpnd_menu").css("display", "flex");
+    $("#main").stop().animate({ opacity: 0.10 }, 1000);
+    $("#particles-js-2").stop().animate({ opacity: 0.10 }, 1000);
+    });
+ });
  $(document).ready(function(){
   $(".backbtn").click(function(){
        $(".projects, .profile, .contact").fadeOut(1);
@@ -53,42 +55,6 @@ $(document).ready(function(){
        $("#particles-js").fadeIn(2000);
        $('#particles-js').css('transform', 'scale(1)');
        $("#main").css("z-index", "2");
-    });
- });
- $(document).ready(function(){
-  $(".menubar1").click(function(){
-    $(".menubar").fadeOut(1, function(){
-      $(".menubar").fadeIn(500);
-      $(".menubar1").hide(0);
-      $(".menubar2").show(0);
-      $(".menubar3").show(0);
-      $(".projects").fadeIn(1000);
-      $(".profile, .contact").fadeOut(1);
-    });
-    });
- });
- $(document).ready(function(){
-  $(".menubar2").click(function(){
-    $(".menubar").fadeOut(1, function(){
-      $(".menubar").fadeIn(500);
-      $(".menubar1").show(0);
-      $(".menubar2").hide(0);
-      $(".menubar3").show(0);
-      $(".profile").fadeIn(1000);
-      $(".projects, .contact").fadeOut(1);
-    });
-    });
- });
- $(document).ready(function(){
-  $(".menubar3").click(function(){
-    $(".menubar").fadeOut(1, function(){
-      $(".menubar").fadeIn(500);
-      $(".menubar1").show(0);
-      $(".menubar2").show(0);
-      $(".menubar3").hide(0);
-      $(".contact").fadeIn(1000);
-      $(".projects, .profile").fadeOut(1);
-    });
     });
  });
 
